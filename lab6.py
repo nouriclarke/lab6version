de7316be405a6a821a4040bcd1af32498c66b42a
Nouri Clarke <clarkenouri@ufl.edu>
2023-10-24 13:45:21 -0400

    Created encode function

---
+++ lab6.py
@@ -0,0 +1,33 @@
+# Nouri Clarke
+def encode(password):
+    var = ''
+    for i in password:
+        result = int(i)
+        result += 3
+        if result >= 10:
+            result %= 10
+        var += str(result)
+    return var
+
+
+if __name__ == "__main__":
+    while True:
+        print("""
+        Menu
+        -------------
+        1. Encode
+        2. Decode
+        3. Exit""")
+        print()
+        choice = input("Please enter an option: ")
+
+        if choice == "1":
+            password = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!")
+            print(encode(password))
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            break
+        else:
+            print("Invalid option, please try again")
