5b44409cfe5b9f14b49d02ad230586c9d1436393
Charlie <cvmcharlie@gmail.com>
2023-10-26 11:20:42 -0400

    created and implemented decode function and function call in main method

--- lab6.py
+++ lab6.py
@@ -9,6 +9,16 @@ def encode(password):
         var += str(result)
     return var
 
+def decode(encrypt_pass):
+    decoded_pass = ''
+    for char in encrypt_pass:
+        decode_val = int(char) - 3
+        if decode_val < 0:
+            decode_dict = {-3:'7',-2:'8',-1:'9'}
+            decode_val = decode_dict[decode_val]
+        decoded_pass += str(decode_val)
+    return decoded_pass
+
 
 if __name__ == "__main__":
     while True:
@@ -26,7 +36,8 @@ if __name__ == "__main__":
             print("Your password has been encoded and stored!")
             encoded_pass = encode(password)
         elif choice == "2":
-            pass
+            decoded_pass = decode(encoded_pass)
+            print(f'The encoded password is {encoded_pass}, and the original password is {decoded_pass}')
         elif choice == "3":
             break
         else:
